0a4c41b41f22eab544f0103f3e0f3d17864fb46f
aysar <aysarhajaj@gmail.com>
2023-03-03 01:03:58 +0200

    add create and create token functions

--- backend/app/Repositories/AuthRepository.php
+++ backend/app/Repositories/AuthRepository.php
@@ -2,6 +2,36 @@
 
 namespace App\Repositories;
 
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
+
 class AuthRepository
 {
+    public function create($input)
+    {
+        DB::beginTransaction();
+        try {
+            $user = User::create($input);
+
+            DB::commit();
+            return $user;
+        } catch (\Throwable $th) {
+            DB::rollBack();
+            throw $th;
+        }
+    }
+
+    public function createToken($user)
+    {
+        DB::beginTransaction();
+        try {
+            $token = $user->createToken('product-module')->accessToken;
+
+            DB::commit();
+            return $token;
+        } catch (\Throwable $th) {
+            DB::rollBack();
+            throw $th;
+        }
+    }
 }
