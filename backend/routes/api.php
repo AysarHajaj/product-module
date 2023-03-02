f374b8feaa94ef1d046f4b81eb9fcecdb15852e0
aysar <aysarhajaj@gmail.com>
2023-03-03 01:53:13 +0200

    add logout route

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
+Route::middleware('auth:api')->group(function () {
+    Route::get(
+        'logout',
+        [AuthController::class, 'logout']
+    );
+});
