be7e928adfeb05d6cabea1b53012d61c24f6ade2
aysar <aysarhajaj@gmail.com>
2023-03-02 23:47:14 +0200

    Add AuthRepository

--- backend/app/Services/AuthService.php
+++ backend/app/Services/AuthService.php
@@ -2,6 +2,14 @@
 
 namespace App\Services;
 
+use App\Repositories\AuthRepository;
+
 class AuthService
 {
+    private $authRepository;
+
+    public function __construct(AuthRepository $authRepository)
+    {
+        $this->authRepository = $authRepository;
+    }
 }
