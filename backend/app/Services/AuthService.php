3d3c6ba96619f5d322cdbdcf7a46ddde2d3ad355
aysar <aysarhajaj@gmail.com>
2023-03-03 01:04:17 +0200

    add register function

--- backend/app/Services/AuthService.php
+++ backend/app/Services/AuthService.php
@@ -2,14 +2,46 @@
 
 namespace App\Services;
 
+use App\Formatters\AuthFormatter;
 use App\Repositories\AuthRepository;
 
-class AuthService
+class AuthService extends Service
 {
     private $authRepository;
+    private $authFormatter;
 
-    public function __construct(AuthRepository $authRepository)
+    public function __construct(AuthRepository $authRepository, AuthFormatter $authFormatter)
     {
         $this->authRepository = $authRepository;
+        $this->authFormatter = $authFormatter;
+    }
+
+    public function register($input)
+    {
+        try {
+            $input['password'] = bcrypt($input['password']);
+            $user = $this->authRepository->create($input);
+            $token = $this->authRepository->createToken($user);
+            $user = $this->authFormatter->formatUser($user);
+
+            $result = $this->authFormatter->prepareSuccessResponse();
+            $result = $this->authFormatter->addDataToResponse(
+                $result,
+                'user',
+                $user
+            );
+            $result = $this->authFormatter->addDataToResponse(
+                $result,
+                'token',
+                $token
+            );
+
+            return $this->getResponse($result, 200);
+        } catch (\Throwable $th) {
+            return $this->getResponse(
+                $this->authFormatter->errorResponseData($th->getMessage()),
+                500
+            );
+        }
     }
 }
