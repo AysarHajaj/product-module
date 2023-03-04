fda6a3a61bbe7dcbe7320d464b3ce624daeff95e
aysar <aysarhajaj@gmail.com>
2023-03-04 02:17:08 +0200

    add formatProduct and formatProducts functions

--- backend/app/Formatters/ProductFormatter.php
+++ backend/app/Formatters/ProductFormatter.php
@@ -2,6 +2,26 @@
 
 namespace App\Formatters;
 
+use App\Constants\ProductTypes;
+
 class ProductFormatter extends Formatter
 {
+    public function formatProducts($products)
+    {
+        $result = [];
+        foreach ($products as $product) {
+            $result[] = $this->formatProduct($product);
+        }
+
+        return $result;
+    }
+
+    public function formatProduct($product)
+    {
+        if (isset($product->type)) {
+            $product['type'] = ProductTypes::ALL[$product['type']]['name'];
+        }
+
+        return $product;
+    }
 }
