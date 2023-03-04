892f0467ce54b4597f1d29f607fcfb970b2f64e2
aysar <aysarhajaj@gmail.com>
2023-03-04 02:17:38 +0200

    add all function

--- backend/app/Repositories/ProductRepository.php
+++ backend/app/Repositories/ProductRepository.php
@@ -6,4 +6,25 @@ use App\Models\Product;
 
 class ProductRepository
 {
+    public function all()
+    {
+        $products = Product::select(
+            'products.id',
+            'products.name',
+            'products.price',
+            'products.type',
+            'products.user_id',
+            'products.deactivated_at',
+            'products.created_at',
+        )->with([
+            'user' => function ($q) {
+                $q->select(
+                    'users.id',
+                    'users.name'
+                );
+            }
+        ])->get();
+
+        return $products;
+    }
 }
