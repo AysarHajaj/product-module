7c51ae3e0eeaaacb88dd1e7db8a0945e67d63d93
aysar <aysarhajaj@gmail.com>
2023-03-05 01:30:24 +0200

    add loggings relation

--- backend/app/Models/Product.php
+++ backend/app/Models/Product.php
@@ -22,4 +22,9 @@ class Product extends Model
     {
         return $this->belongsTo(User::class, 'user_id');
     }
+
+    public function loggings()
+    {
+        return $this->hasMany(Logging::class, 'user_id');
+    }
 }
