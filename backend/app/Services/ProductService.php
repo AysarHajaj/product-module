6a79b8097502e17226110785d0ceef3425de46f1
aysar <aysarhajaj@gmail.com>
2023-03-04 02:53:22 +0200

    add store function

--- backend/app/Services/ProductService.php
+++ backend/app/Services/ProductService.php
@@ -3,6 +3,7 @@
 namespace App\Services;
 
 use App\Formatters\ProductFormatter;
+use App\Repositories\AuthRepository;
 use App\Repositories\ProductRepository;
 use Illuminate\Support\Facades\DB;
 
@@ -10,13 +11,16 @@ class ProductService extends Service
 {
     private $productRepository;
     private $productFormatter;
+    private $authRepository;
 
     public function __construct(
         ProductRepository $productRepository,
-        ProductFormatter $productFormatter
+        ProductFormatter $productFormatter,
+        AuthRepository $authRepository
     ) {
         $this->productRepository = $productRepository;
         $this->productFormatter = $productFormatter;
+        $this->authRepository = $authRepository;
     }
 
     public function getAll()
@@ -26,6 +30,26 @@ class ProductService extends Service
             $products = $this->productRepository->all();
             $formattedProducts = $this->productFormatter->formatProducts($products);
             $result = $this->productFormatter->successResponseData($formattedProducts);
+
+            DB::commit();
+            return $this->getResponse($result, 200);
+        } catch (\Throwable $th) {
+            DB::rollBack();
+            return $this->getResponse(
+                $this->productFormatter->errorResponseData($th->getMessage()),
+                500
+            );
+        }
+    }
+
+    public function store($input)
+    {
+        DB::beginTransaction();
+        try {
+            $user = $this->authRepository->authUser();
+            $input = $this->productFormatter->prepareInputData($input, $user);
+            $this->productRepository->create($input);
+            $result = $this->productFormatter->successResponseData(true);
 
             DB::commit();
             return $this->getResponse($result, 200);
