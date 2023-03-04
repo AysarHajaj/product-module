47f5d39a10318e5ff434c2f6572bb39714127c8a
aysar <aysarhajaj@gmail.com>
2023-03-04 16:18:54 +0200

    add filter route

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -30,6 +30,7 @@ Route::middleware('auth:api')->group(function () {
     Route::prefix('products')->group(function () {
         Route::get('/', [ProductController::class, 'index']);
         Route::post('/', [ProductController::class, 'store']);
+        Route::get('/filter', [ProductController::class, 'filter']);
         Route::get('/{id}', [ProductController::class, 'show']);
         Route::put('/{id}', [ProductController::class, 'update']);
         Route::delete('/{id}', [ProductController::class, 'destroy']);
